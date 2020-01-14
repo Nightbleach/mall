dd9a85cc013d76dbbc9015024f7f896f6e29b5fd
Terrynightbleach <terrynightbleach@gmail.com>
2020-01-14 15:49:09 +1300

    Project init

---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+   <h1>1234</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
